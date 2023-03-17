da7351ab97dfbc9e975c696fe40400879270fe67
809 <1195800137@qq.com>
2023-03-17 12:13:38 +0800

    2023年3月17日

--- stable_baselines/enjoy.py
+++ stable_baselines/enjoy.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
 
     model_name = args.model_name + "_"
      # 存放在sb3model/文件夹下
-    save_path = f"sb3model/{env_id}/{model_name}{args.algo}_{env_id}.zip"
+    save_path = f"sb3model/{env_id}/{model_name}{args.algo}_{env_id}/best_model.zip"
 
     if not os.path.isfile(save_path) or args.load_best:
         print("Loading best model")

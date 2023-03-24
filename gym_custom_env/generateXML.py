2b72aa5d648c0090abc26576a83e84e291ce05db
lenovo 3050ti <1195800137@qq.com>
2023-03-24 19:36:28 +0800

    自定义XML

--- gym_custom_env/generateXML.py
+++ gym_custom_env/generateXML.py
@@ -69,7 +69,7 @@ class XMLPart():
         
 
 
-class XMLTree():
+class MujocoXML():
     '''
     维护一个XML树
     '''
@@ -79,11 +79,22 @@ class XMLTree():
 
     ):
         # 生成根节点
-        self.root = XMLPart(root_tag, root = True, attributes={"mudel":"humanoid"} )
-        self.elements = dict() # 根节点所包含的子节点的元素集，只包括往下一级的子节点，便于查找
+        self.root = XMLPart(root_tag, root = True, attributes={"model":"humanoid"} )
+        self.elements : Dict[str, XMLPart] = {} # 根节点所包含的子节点的元素集，只包括往下一级的子节点，便于查找
         self.texture = list()
+        self.humanoid_init_pos = "0 0 1.4" # humanoid出生位置
 
-    def add_compiler(self, tag = "compiler", attributes=None) -> None:
+    def list2str(self, l):
+        '''
+        将输入的列表转换为带空格的字符串
+        '''
+        if type(l) != list:
+            return str(l)
+        str_list = [str(num) for num in l] 
+        result_str = " ".join(str_list) 
+        return result_str
+
+    def _add_compiler(self, tag = "compiler", attributes=None) -> None:
         # 添加默认compiler参数
         compiler_attr = {"angle":"degree",
                          "inertiafromgeom":"true"}
@@ -94,7 +105,7 @@ class XMLTree():
         compiler = self.root.child_element(tag=tag, attributes=compiler_attr)
         self.elements.update({tag:compiler})  # 添加到元素集中
 
-    def add_default(self, tag = "default", attributes=None) -> None:
+    def _add_default(self, tag = "default", attributes=None) -> None:
         default = self.root.child_element(tag=tag,attributes={})
         if attributes :
             self.elements.update({tag:default})  # 添加到元素集中
@@ -113,19 +124,18 @@ class XMLTree():
                              "material":"geom",
                              "rgba":"0.8 0.6 .4 1"
                              }
-        default_geom_tag = "joint"
+        default_geom_tag = "geom"
         defalut_geom = default.child_element(tag=default_geom_tag, 
                                              attributes=default_geom_attr)
         #motor
         default_motor_attr = {"ctrllimited":"true",
                              "ctrlrange":"-.4 .4",
                              }
-        default_motor_tag = "joint"
+        default_motor_tag = "motor"
         defalut_motor = default.child_element(tag=default_motor_tag,
                                               attributes=default_motor_attr)
 
-
-    def add_option(self, tag = "option", attributes=None):
+    def _add_option(self, tag = "option", attributes=None):
         '''
         添加option项
         '''
@@ -133,15 +143,16 @@ class XMLTree():
                         "iterations":"50",
                         "solver":"PGS",
                         "timestep":"0.003",
+                        "gravity":"0 0 0"
                       }  
         if attributes :
             option_attr.update(attributes)      
         option = self.root.child_element(tag = tag, attributes= option_attr)
         self.elements.update({tag:option})  # 添加到元素集中
 
-    def add_size(self, tag = "size",attributes=None):
+    def _add_size(self, tag = "size",attributes=None):
         '''
-        TODO: 添加size元素   <size nkey="5" nuser_geom="1"/>
+        添加size元素
         '''
         size_attr = {"nkey":"5",
                      "nuser_geom":"1",
@@ -151,13 +162,9 @@ class XMLTree():
         size = self.root.child_element(tag = tag, attributes= size_attr)
         self.elements.update({tag:size})  # 添加到元素集中
 
-
-    def add_visual(self, tag="visual",attributes=None):
+    def _add_visual(self, tag="visual",attributes=None):
         '''
-        TODO: 添加visual元素 
-        <visual>
-            <map fogend="5" fogstart="3"/>
-        </visual>
+        添加visual元素 
         '''
         visual_attr = {}
         if attributes :    
@@ -171,9 +178,9 @@ class XMLTree():
         visual_map = visual.child_element(tag=visual_map_tag,
                                               attributes=visual_map_attr)   
 
-    def add_asset(self, tag="asset",attributes=None):
+    def _add_asset(self, tag="asset",attributes=None):
         '''
-        TODO: 添加asset元素
+        添加asset元素
         '''
         asset_attr = {}
         if attributes :
@@ -225,11 +232,66 @@ class XMLTree():
         geom_attr     = { "name":"geom",
                           "texture":"texgeom",
                           "texuniform":"true",
-                          "specular":"1",
-                          "texrepeat":"60 60",
-                          "texture":"texplane",
                           }# flat
         asset_material_geom = asset.child_element("material",geom_attr)
+
+    def add_geom(self, 
+                parent:XMLPart,
+                name: str,
+                geom_type: str,
+                size: List[Union[float,int]],
+                pos: Optional[List[Union[float,int]]] = None,
+                from_point: Optional[List[Union[float,int]]] = None, 
+                to_point: Optional[List[Union[float,int]]] = None,
+                user: Optional[List[Union[float,int]]] = None
+            ) -> XMLPart:
+        '''
+        创建geom元素需要的参数字典
+        '''
+        geom_attr = {}
+        geom_attr["name"]=name
+        geom_attr["type"]=geom_type
+        geom_attr["size"]=self.list2str(size)
+        if geom_type == "sphere":  # 如果是sphere，参数为pos
+            assert pos, "sphere类geom缺少参数pos"
+            geom_attr["pos"] = self.list2str(pos)
+        if geom_type == "capsule": # 如果是capsule，参数为fromto
+            assert from_point!=None and to_point!=None , "capsule类geom缺少参数fromto"
+            geom_attr["fromto"] = self.list2str(from_point) + " " + self.list2str(to_point)
+        if user:
+            geom_attr["user"] = self.list2str(user)
+        geom_part = parent.child_element('geom',geom_attr)
+        return geom_part
+
+    def add_joint(  self, 
+                    parent: XMLPart,
+                    name: str,
+                    joint_type: str,
+                    armature: Optional[float] = None,
+                    axis: Optional[List[Union[float,int]]] = None,
+                    pos: Optional[List[Union[float,int]]] = None,
+                    joint_range: Optional[List[Union[float,int]]] = None, 
+                    stiffness: Optional[List[Union[float,int]]] = None,
+                    damping: Optional[List[Union[float,int]]] = None,
+                    limited: str = None,
+                    ) -> XMLPart:
+        '''
+        创建joint元素需要的参数字典
+        '''
+        joint_attr = {}
+        joint_attr['name'] = name
+        joint_attr['type'] = joint_type
+        joint_attr['armature'] = self.list2str(armature)
+        if joint_type == 'free': 
+            joint_attr['limited'] = limited
+        else:
+            joint_attr['axis'] = self.list2str(axis)
+        joint_attr['pos'] = self.list2str(pos)
+        if joint_range: joint_attr['range'] = self.list2str(joint_range)
+        if stiffness: joint_attr['stiffness'] = self.list2str(stiffness) 
+        if damping: joint_attr['damping'] = self.list2str(damping) 
+        joint_part = parent.child_element('joint',joint_attr)
+        return joint_part
 
     def pretty_xml(self) -> None:
         prettyXml(self.root.xmlpart)
@@ -245,20 +307,127 @@ class XMLTree():
 TODO: 
     定义HumanoidXML类，继承XMLTree类，定义具有人形机器人的XML模型文档。
 '''
-class HumanoidXML(XMLTree):
+class HumanoidXML(MujocoXML):
     def __init__(self, 
                  root_tag:str = "mujoco"):
-        super(HumanoidXML,self).__init__(root_tag=root_tag)
-        
-    def base_structure(self,):
+        super(HumanoidXML ,self).__init__(root_tag=root_tag)
+        self.param_list = { 'head_radius' : 0.18,
+                            'torso_lenth': 0.34,
+        }
+
+    def _basic_structure(self):
         '''
         生成XML文档的基本框架
         '''
-        self.add_compiler()
-        self.add_default()
-        self.add_option()
-        self.add_visual()
-        self.add_asset()
+        self._add_compiler()
+        self._add_default()
+        self._add_option()
+        self._add_visual()
+        self._add_asset()
+        self._add_worldbody()
+    
+    def _add_worldbody(self, 
+                      born_position: List[Union[float,int]] = [0.0,0.0,1.4],
+                      tag = "worldbody"
+                      ):
+        worldbody = self.root.child_element(tag,attributes={})
+        self.elements.update({tag:worldbody})
+        light_attr = {"cutoff":"100",
+                      "diffuse":"1 1 1",
+                      "dir":"-0 0 -1.3",
+                      "directional":"true",
+                      "exponent":"1",
+                      "pos":"0 0 1.3",
+                      "specular":".1 .1 .1",        } 
+        worldbody_light = worldbody.child_element("light",light_attr)
+
+    def set_terrain(self, 
+                    terrain_type="default", 
+                    name: Union[str, None] = None , 
+                    size: List[str] = None):
+        '''
+        定义地形，为worldbody的子元素
+        '''
+        tag = "geom"
+        if terrain_type=="default":
+            if not name:
+                name = "flatfloor" 
+            if not size:
+                Size = "20 20 0.125"
+            else:
+                Size = self.list2str(size)
+            terrain_attr = {"condim":"3",
+                            "friction":"1 .1 .1",
+                            "material":"MatPlane",
+                            "name":name,
+                            "pos":"0 0 0",
+                            "rgba":"0.8 0.9 0.8 1",
+                            "size":Size,
+                            "type":"plane",}
+        self.elements["worldbody"].child_element(tag, terrain_attr) 
+    
+    def _create_torso(self ) -> XMLPart:
+        '''
+        创建躯干
+        '''
+        worldbody = self.elements["worldbody"]
+        torso_attr = {"name":"torso", "pos":self.humanoid_init_pos}
+        torso = worldbody.child_element("body", torso_attr)  # 躯干
+        camera_attr = {'name':'track',
+                       'mode':'trackcom',
+                       'pos':'0 -4 0',
+                       'xyaxes':'1 0 0 0 0 1',}
+        camera = torso.child_element("camera",camera_attr)
+        root_joint = self.add_joint(name='root',
+                                     parent = torso,
+                                     joint_type='free', 
+                                     armature = 0 , 
+                                     damping = 0, 
+                                     limited = 'false',
+                                     pos = [0,0,0],
+                                     stiffness = 0 )
+        
+        torso_geom = self.add_geom(name = 'torso_geom',
+                                   parent = torso,
+                                   geom_type = 'capsule',
+                                   from_point = [0, -self.param_list['torso_lenth']/2, 0],
+                                   to_point = [0, self.param_list['torso_lenth']/2, 0],
+                                   size = 0.07 )
+        head_geom = self.add_geom(name = 'head_geom',
+                                  parent = torso,
+                                  geom_type = 'sphere',
+                                  pos = [0, 0, self.param_list['head_radius']*1.1],
+                                  size = self.param_list['head_radius']/2,
+                                  user = 258)       
+        uwaist_geom = self.add_geom(name='uwaist_geom',
+                                    parent = torso,
+                                    size = 0.06,
+                                    geom_type = 'capsule',
+                                    from_point= [-0.01, -0.06, -0.12],
+                                    to_point= [-0.01, 0.06, -0.12])
+        return torso
+
+    def create_humanoid(self,):
+        '''
+        创建humanoid机器人
+        '''
+        torso = self._create_torso()  
+        
+    def write_xml(self, file_path = 'humanoid.xml'):
+        '''
+        输出xml文档
+        '''
+        self._basic_structure() # 生成基本框架
+        self.set_terrain() # 生成地形
+        self.create_humanoid() # 生成机器人
+        self.generate(file_path)
+
+    def set_params(self, **params):
+        pass
+
+    def update(self, **params):
+        pass
+
 
 if __name__ == "__main__":
     """     
@@ -271,5 +440,4 @@ if __name__ == "__main__":
     t.generate() 
     """
     t = HumanoidXML()
-    t.base_structure()
-    t.generate(file_path="e.xml")
+    t.write_xml(file_path="e.xml")
--- learning/checkXML.py
+++ learning/checkXML.py
@@ -6,16 +6,17 @@ python learning/checkXML.py
 # 手动添加mujoco路径
 import os
 from getpass import getuser
+import time
 user_id = getuser()
 os.add_dll_directory(f"C://Users//{user_id}//.mujoco//mujoco200//bin")
 os.add_dll_directory(f"C://Users//{user_id}//.mujoco//mujoco-py-2.0.2.0//mujoco_py")
 # -------------------------------------------------------
 
 import mujoco_py
-
+import numpy as np
 # 加载 XML 文件
 # model = mujoco_py.load_model_from_path("gym_custom_env\\assets\\humanoid_custom.xml")
-model = mujoco_py.load_model_from_path("humanoid.xml")
+model = mujoco_py.load_model_from_path("e.xml")
 
 # 创建仿真环境和渲染器
 sim = mujoco_py.MjSim(model)
@@ -25,6 +26,10 @@ viewer = mujoco_py.MjViewer(sim)
 dt = 0.01
 timesteps = 5000
 viewer.render()
+
+""" ctrl = np.zeros(17)
+ctrl[12] = 0.0
+sim.data.ctrl[:] = ctrl """
 # 运行仿真并在每个时间步骤中进行渲染
 for i in range(timesteps):
     sim.step()

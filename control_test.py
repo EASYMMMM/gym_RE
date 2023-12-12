5005ef0e962029ad9cb6535cfd8b3ab805fa3728
lenovo 3050ti <1195800137@qq.com>
2023-12-12 20:23:35 +0800

    debug

--- control_test.py
+++ control_test.py
@@ -13,10 +13,6 @@ python control_test.py --algo ppo --env TranslationOscillatorEnv-v0  --model-nam
 # 手动添加mujoco路径
 import os
 from getpass import getuser
-user_id = getuser()
-os.add_dll_directory(f"C://Users//{user_id}//.mujoco//mujoco200//bin")
-os.add_dll_directory(f"C://Users//{user_id}//.mujoco//mujoco-py-2.0.2.0//mujoco_py")
-# -------------------------------------------------------
 import time
 import argparse
 import sys
@@ -24,7 +20,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import gym_custom_env       # 注册自定义环境
 import gym
 import numpy as np
-import pybullet_envs
 from gym_custom_env.TranslationOscillator import play
 from stable_baselines3 import SAC, TD3, PPO
 

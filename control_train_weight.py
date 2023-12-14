659508a495a9641818fe8eb560bf12c0529a789a
easym <1195800137@qq.com>
2023-12-14 14:47:52 +0800

    'wr'

--- control_train_weight.py
+++ control_train_weight.py
@@ -41,17 +41,17 @@ if __name__ == "__main__":
     # 环境名
     env_id = 'TranslationOscillatorEnv-v0'
     n_timesteps = 1000000
-    model_name = 't1_wr41_NoSquare'+ "_"  #41 表示4 0.4 1 0.1
+    model_name = 't1_wr41_Square'+ "_"  #41 表示4 0.4 1 0.1
     algo = 'ppo'
     # 存放在sb3model/文件夹下
     save_path = f"sb3model/{env_id}/{model_name}{algo}_{env_id}"
 
     # tensorboard log 路径
-    tensorboard_log_path = f"tensorboard_log/{env_id}/{model_name}{algo}_{env_id}/weight_R"
+    tensorboard_log_path = f"tensorboard_log/{env_id}/weight_R/{model_name}{algo}_{env_id}"
     tensorboard_log_name = f"{model_name}{algo}_{env_id}"
 
     # Instantiate and wrap the environment
-    env = gym.make(env_id,suqare_reward=False , reward_weight = [4,0.4,1,0.1])
+    env = gym.make(env_id,suqare_reward=True , reward_weight = [4,0.4,1,0.1])
 
     # Create the evaluation environment and callbacks
     eval_env = Monitor(gym.make(env_id,suqare_reward=True))
@@ -109,8 +109,8 @@ if __name__ == "__main__":
 
 
 #####################################################################################
-    # 8倍
-    model_name = 't1_wr81_NoSquare'+ "_"
+    # 2倍
+    model_name = 't1_wr21_NoSquare'+ "_"
     # 存放在sb3model/文件夹下
     save_path = f"sb3model/{env_id}/{model_name}{algo}_{env_id}"
 
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     tensorboard_log_name = f"{model_name}{algo}_{env_id}"
 
     # Instantiate and wrap the environment
-    env = gym.make(env_id,suqare_reward=False, reward_weight = [8,0.8,1,0.1])
+    env = gym.make(env_id,suqare_reward=False, reward_weight = [2,0.2,1,0.1])
     # Create the evaluation environment and callbacks
     eval_env = Monitor(gym.make(env_id,suqare_reward=False))
     begin_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())

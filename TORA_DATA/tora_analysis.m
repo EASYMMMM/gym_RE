f4b2d3675ab32d7a5c6d18727f74ed7af85d37ff
lenovo 3050ti <1195800137@qq.com>
2023-12-16 01:29:25 +0800

    困死了

--- TORA_DATA/tora_analysis.m
+++ TORA_DATA/tora_analysis.m
@@ -1,38 +1,45 @@
-T1 = readtable('t1_Square_ppo_.csv');
-T2 = readtable('t1_NoSquare_ppo_.csv');
-T3 = readtable('t1_Square_sac_.csv');
-T4 = readtable('t1_NoSquare_sac_.csv');
-T5 = readtable('t1_wr41_Square_ppo_.csv');
-T6 = readtable('t1_wr41_NoSquare_ppo_.csv');
+T1_name = 't1_wr21_Square_acc_ppo_';
+T2_name = 't1_wr41_Square_acc_ppo_';
+T3_name = 't1_wr41_Square_acc_sr1_randinit_ppo_';
+T4_name = 't1_wr41_Square_acc_sr2_randinit_ppo_';
+T5_name = 't1_wr41_Square_acc_sr1_0init_ppo_';
+T6_name = 't1_wr41_Square_acc_sr2_0init_ppo_';
+
+T1 = readtable([T1_name,'.csv']);
+T2 = readtable([T2_name,'.csv']);
+T3 = readtable([T3_name,'.csv']);
+T4 = readtable([T4_name,'.csv']);
+T5 = readtable([T5_name,'.csv']);
+T6 = readtable([T6_name,'.csv']);
 
 
-drawT1 = 1;
-drawT2 = 1;
-drawT3 = 1;
-drawT4 = 0;
-drawT5 = 1;
-drawT6 = 0;
+drawT1 = 1  ;
+drawT2 = 1  ;
+drawT3 = 0  ;
+drawT4 = 0  ;
+drawT5 = 0  ;
+drawT6 = 0  ;
 %% 
 figure(1)
 subplot(2,2,1)
 if drawT1
-    plot(T1.x1,'Linewidth',2,'DisplayName','Square ppo');
+    plot(T1.x1,'Linewidth',2,'DisplayName',T1_name);
 end
 hold on
 if drawT2
-    plot(T2.x1,'Linewidth',2,'DisplayName','NoSquare ppo');
+    plot(T2.x1,'Linewidth',2,'DisplayName',T2_name);
 end
 if drawT3
-    plot(T3.x1,'Linewidth',2,'DisplayName','Square sac');
+    plot(T3.x1,'Linewidth',2,'DisplayName',T3_name);
 end
 if drawT4
-    plot(T4.x1,'Linewidth',2,'DisplayName','NoSquare sac');
+    plot(T4.x1,'Linewidth',2,'DisplayName',T4_name);
 end
 if drawT5
-    plot(T5.x1,'Linewidth',2,'DisplayName','wr41 Square ppo');
+    plot(T5.x1,'Linewidth',2,'DisplayName',T5_name);
 end
 if drawT6
-    plot(T6.x1,'Linewidth',2,'DisplayName','wr41 NoSquare ppo');
+    plot(T6.x1,'Linewidth',2,'DisplayName',T6_name);
 end
 title('x1')
 legend()
@@ -40,23 +47,23 @@ grid on
 
 subplot(2,2,2)
 if drawT1
-    plot(T1.x2,'Linewidth',2,'DisplayName','Square ppo');
+    plot(T1.x2,'Linewidth',2,'DisplayName',T1_name);
 end
 hold on
 if drawT2
-    plot(T2.x2,'Linewidth',2,'DisplayName','NoSquare ppo');
+    plot(T2.x2,'Linewidth',2,'DisplayName',T2_name);
 end
 if drawT3
-    plot(T3.x2,'Linewidth',2,'DisplayName','Square sac');
+    plot(T3.x2,'Linewidth',2,'DisplayName',T3_name);
 end
 if drawT4
-    plot(T4.x2,'Linewidth',2,'DisplayName','NoSquare sac');
+    plot(T4.x2,'Linewidth',2,'DisplayName',T4_name);
 end
 if drawT5
-    plot(T5.x2,'Linewidth',2,'DisplayName','wr41 Square ppo');
+    plot(T5.x2,'Linewidth',2,'DisplayName',T5_name);
 end
 if drawT6
-    plot(T6.x2,'Linewidth',2,'DisplayName','wr41 NoSquare ppo');
+    plot(T6.x2,'Linewidth',2,'DisplayName',T6_name);
 end
 title('x2')
 legend()
@@ -65,23 +72,23 @@ grid on
 
 subplot(2,2,3)
 if drawT1
-    plot(T1.x3,'Linewidth',2,'DisplayName','Square ppo');
+    plot(T1.x3,'Linewidth',2,'DisplayName',T1_name);
 end
 hold on
 if drawT2
-    plot(T2.x3,'Linewidth',2,'DisplayName','NoSquare ppo');
+    plot(T2.x3,'Linewidth',2,'DisplayName',T2_name);
 end
 if drawT3
-    plot(T3.x3,'Linewidth',2,'DisplayName','Square sac');
+    plot(T3.x3,'Linewidth',2,'DisplayName',T3_name);
 end
 if drawT4
-    plot(T4.x3,'Linewidth',2,'DisplayName','NoSquare sac');
+    plot(T4.x3,'Linewidth',2,'DisplayName',T4_name);
 end
 if drawT5
-    plot(T5.x3,'Linewidth',2,'DisplayName','wr41 Square ppo');
+    plot(T5.x3,'Linewidth',2,'DisplayName',T5_name);
 end
 if drawT6
-    plot(T6.x3,'Linewidth',2,'DisplayName','wr41 NoSquare ppo');
+    plot(T6.x3,'Linewidth',2,'DisplayName',T6_name);
 end
 title('x3')
 legend()
@@ -89,23 +96,23 @@ grid on
 
 subplot(2,2,4)
 if drawT1
-    plot(T1.x4,'Linewidth',2,'DisplayName','Square ppo');
+    plot(T1.x4,'Linewidth',2,'DisplayName',T1_name);
 end
 hold on
 if drawT2
-    plot(T2.x4,'Linewidth',2,'DisplayName','NoSquare ppo');
+    plot(T2.x4,'Linewidth',2,'DisplayName',T2_name);
 end
 if drawT3
-    plot(T3.x4,'Linewidth',2,'DisplayName','Square sac');
+    plot(T3.x4,'Linewidth',2,'DisplayName',T3_name);
 end
 if drawT4
-    plot(T4.x4,'Linewidth',2,'DisplayName','NoSquare sac');
+    plot(T4.x4,'Linewidth',2,'DisplayName',T4_name);
 end
 if drawT5
-    plot(T5.x4,'Linewidth',2,'DisplayName','wr41 Square ppo');
+    plot(T5.x4,'Linewidth',2,'DisplayName',T5_name);
 end
 if drawT6
-    plot(T6.x4,'Linewidth',2,'DisplayName','wr41 NoSquare ppo');
+    plot(T6.x4,'Linewidth',2,'DisplayName',T6_name);
 end
 title('x4')
 legend()
